902d0e9042d438002b55a306fbff86689fc23e74
Kurt King <kurtaking@gmail.com>
2017-05-27 02:09:16 -0500

    User can now add recipe ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,47 @@
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/models/ingredient.model'
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(
+    private shoppingListService : ShoppingListService
+  ) {}
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe1', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A Test Recipe2', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A Test Recipe3', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe(
+      'Burgers',
+      'Recipe for my making homemade burgers',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 2),
+        new Ingredient('Cheese', 1)
+      ]
+    ),
+    new Recipe(
+      'Tacos',
+      'Best taco recipe in town',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Tortillas', 4),
+        new Ingredient('Carne Asada', 1),
+        new Ingredient('Onion', 1),
+        new Ingredient('Salsa', 1)
+      ]
+    )
   ];
 
   getRecipes() {
     return this.recipes.slice(); // only allow a copy to be accessed from outside component
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 
 }
