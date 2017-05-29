c4a9824e6d6a83dff2ac1bf2a1a540a0c4bd7d22
Kurt King <kurtaking@gmail.com>
2017-05-28 23:46:41 -0500

    Added a CoreModule for cross site components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule }         from "./app-routing.module";
 import { ShoppingListModule }       from "./shopping-list/shopping-list.module";
 import { SharedModule }             from "./shared/modules/shared.module";
 import { AuthModule }               from "./auth/auth.module";
+import { AccountModule }            from "./account/account.module";
+import { CoreModule }               from "./core/core.module";
 
 import { AppComponent }             from './app.component';
-import { HeaderComponent }          from "./header/header.component";
 
 import { ShoppingListService }      from "./shopping-list/shopping-list.service";
 import { RecipeService }            from "./recipes/recipe.service";
@@ -16,16 +17,10 @@ import { DataStorageService }       from "./shared/services/data-storage.service
 
 import { AuthService }              from "./auth/auth.service";
 import { AuthGuard }                from "./auth/auth-guard.service";
-import { HomeComponent } from './home/home.component';
-import { AccountProfileComponent } from './account/account-profile/account-profile.component';
-import { AccountSettingsComponent } from './account/account-settings/account-settings.component';
-import {AccountModule} from "./account/account.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,7 +29,8 @@ import {AccountModule} from "./account/account.module";
     AccountModule,
     ShoppingListModule,
     AuthModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [
     ShoppingListService,
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,13 +1,13 @@
 import {Component} from '@angular/core';
-import {DataStorageService} from "../shared/services/data-storage.service";
+import {DataStorageService} from "../../shared/services/data-storage.service";
 import {Response} from "@angular/http";
-import {AuthService} from "../auth/auth.service";
+import {AuthService} from "../../auth/auth.service";
 import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls : ['./header.component.css'],
+  templateUrl: 'header.component.html',
+  styleUrls : ['header.component.css'],
 })
 
 export class HeaderComponent {
